5ba052bcfa939d09aaf10072e5eccb1c700f29cc
shandar zade <shandarzade358@gmail.com>
2024-07-20 14:06:13 +0530

    add updateDetails updateAvatar changePassword

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -221,9 +221,104 @@ const refreshAccessToken = asyncHandler( async (req, res) => {
     }
 } )
 
+const changeCurrentPassword = asyncHandler( async (req, res) => {
+
+    const {oldPassword, newPassword} = req.body
+    const user = await User.findById(req.user?._id)
+    const isPasswordValid = await isPasswordCorrect(oldPassword) 
+
+    if(!isPasswordValid){
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save( { validateBeforeSave: false })
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            req.user,
+            "password changed Successfully"
+        )
+    )
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User fetched successfully"
+    ))
+})
+
+const updateAccountDetails = asyncHandler( async (req, res) => {
+    const  {firstName, lastName, email} = req.body
+
+    if(!firstName || !email || !lastName) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = User.findByIdAndDelete(
+        req.user?._id, 
+        { $set: {
+            firstName,
+            lastName,
+            email
+        }},
+        {new: true}
+    ).select("-password")
+
+    return res.status(200)
+    .json( new ApiResponse(
+        200,
+        user,
+        "Account details update successfully"
+    ) )
+}
+)
+
+const updateAvatar = asyncHandler( async (req, res) => {
+    const avatarLocalPath = req.file?.path
+    if (!avatarLocalPath){
+        throw new ApiError(400, "avatar file is missing")
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url){
+        throw new ApiError(400, "Error while uploading avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar : avatar.url
+            }
+        },
+        {new: true}
+    ).select( "-password")
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user,
+            "avatar updated successfully"
+        )
+    )
+})
+
+
 export {
     registerUser,
     logoutUser,
     loginUser,
-    refreshAccessToken
+    refreshAccessToken,
+    getCurrentUser,
+    changeCurrentPassword,
+    updateAccountDetails,
+    updateAvatar
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,10 @@ import {
     loginUser,
     registerUser, 
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    updateAccountDetails,
+    updateAvatar,
+    changeCurrentPassword
 } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -22,5 +25,8 @@ router.route("/register").post(
     router.route("/login").post(loginUser)
     router.route("/logout").post(verifyJWT,  logoutUser)
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/update-details").post(updateAccountDetails)
+    router.route("/update-avatar").post(updateAvatar)
+    router.route("/change-password").post(changeCurrentPassword)
 
 export default router
